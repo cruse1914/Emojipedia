1cff229bbdb0ffffd4c3a9996b7f3ebccb8fe03a
cruse1914 <contact.dm07@gmail.com>
2021-03-16 18:25:40 +0530

    Updates

---
+++ precache-manifest.289b7322ac96c5d044377393005eef00.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099e6a2c350da31b180a0aa41849208c",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "3cd517c8a4f56e0f1387",
+    "url": "/Emojipedia/static/js/2.5c980291.chunk.js"
+  },
+  {
+    "revision": "3b969582fb52851c57f5",
+    "url": "/Emojipedia/static/js/main.bf4cd427.chunk.js"
+  },
+  {
+    "revision": "cfac16d2c1959a499de7",
+    "url": "/Emojipedia/static/js/runtime-main.7586e132.js"
+  }
+]);
